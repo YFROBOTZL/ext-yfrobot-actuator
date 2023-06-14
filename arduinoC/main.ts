f7a6ae01b1f7d8567a52f65167f4ed8bac2824ac
yfrobot-zl <yfrobot-zl@foxmail.com>
2023-06-14 14:31:31 +0800

    V0.0.8 MP3_Bug
    ESP32使用MP3模块时，失灵时不灵。

--- arduinoC/main.ts
+++ arduinoC/main.ts
@@ -1005,9 +1005,9 @@ namespace actuator {
         Generator.addInclude(`defineaudioPlaybackFunA`, `// MP3音频播放模块函数-发送数据\n`+
             `void audioPlaybackSendData(int mp3Pin, int addr) {\n`+
             `  digitalWrite(mp3Pin , HIGH);\n`+
-            `  delay(2);           // >2ms\n`+
+            `  delay(1);           // 1ms\n`+
             `  digitalWrite(mp3Pin , LOW);\n`+
-            `  delay(3);           // >2ms\n`+
+            `  delay(5);           // >3ms\n`+
             `  for (int i = 0; i < 8; i++) {\n`+
             `    digitalWrite(mp3Pin, HIGH);\n`+
             `    if (addr & 1) {\n`+
@@ -1060,9 +1060,9 @@ namespace actuator {
         Generator.addInclude(`defineaudioPlaybackFunA`, `// MP3音频播放模块函数-发送数据\n`+
             `void audioPlaybackSendData(int mp3Pin, int addr) {\n`+
             `  digitalWrite(mp3Pin , HIGH);\n`+
-            `  delay(2);           // >2ms\n`+
+            `  delay(1);           // 1ms\n`+
             `  digitalWrite(mp3Pin , LOW);\n`+
-            `  delay(3);           // >2ms\n`+
+            `  delay(5);           // >3ms\n`+
             `  for (int i = 0; i < 8; i++) {\n`+
             `    digitalWrite(mp3Pin, HIGH);\n`+
             `    if (addr & 1) {\n`+
